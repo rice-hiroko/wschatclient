5dd14c8c0bce5130f9054ea447460f8d1aee9384
hypersad <lethaldamage@protonmail.com>
2017-09-23 16:37:42 +0300

    ⬆ 0.0.9
    * Добавлен параметр для выбора комнаты для подключения при запуске клиента

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  // API-ключ пользователя для автоматической авторизации
   userAPIKey: '',
-
-  // Цвет границ элементов и их наименований
-  bordersColor: '',
+  roomToJoin: '#chat',
+  bordersColor: ''
 };
